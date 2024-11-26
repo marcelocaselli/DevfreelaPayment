b8639caa3f92f49ac6a28b12315be24127172819
Marcelo Caselli <marcelinhocaselli@gmail.com>
2024-11-26 11:58:47 -0300

    Update PaymentInfoInputModel.cs

--- Devfreela.Payments.API/Models/PaymentInfoInputModel.cs
+++ Devfreela.Payments.API/Models/PaymentInfoInputModel.cs
@@ -2,12 +2,12 @@
 {
     public class PaymentInfoInputModel
     {
-        public int Id { get; set; }
+        public int IdProject { get; set; }
         public string CreditCardNumer { get; set; }
         public string Cvv { get; set; }
         public string ExpiresAt { get; set; }
         public string FullName { get; set; }
-        //public decimal Amount { get; set; }
+        public decimal Amount { get; set; }
     }
 }
 
